1d1585a1e5f06a900c6654281db53b1fdef650b7
Jordan - Instructor <jordan-instructor@Jordans-MacBook-Pro.local>
2019-10-03 20:34:45 -0400

    First commit

--- 2300-express-signup-login/server.js
+++ 2300-express-signup-login/server.js
@@ -1,27 +1,53 @@
-let express = require("express") 
-let app = express() 
-let multer = require("multer") 
-let upload = multer() 
-let passwordsAssoc = {} 
-app.use('/', express.static(__dirname + '/public')) 
-app.post("/signup", upload.none(), (req, res) => { 
-  console.log("/signup hit", req.body) 
-  let username = req.body.username 
-  let password = req.body.password 
-  passwordsAssoc[username] = password 
-  res.send("<html><body> signup successful </body></html>") 
-}) 
-app.post("/login", upload.none(), (req, res) => { 
-  console.log("/login hit", req.body) 
-  let username = req.body.username 
-  let passwordGiven = req.body.password 
-  let expectedPassword = passwordsAssoc[username] 
-  if (expectedPassword !== passwordGiven) { 
-    res.send("<html><body> invalid username or password </body></html>") 
-    return 
-  } 
-  res.send("<html><body> login successful </body></html>") 
-}) 
-app.listen(4000, () => { 
-  console.log("server started") 
-}) 
+let express = require('express');
+let app = express();
+let multer = require('multer');
+let upload = multer();
+let passwordsAssoc = {};
+let attempts = {};
+app.use('/', express.static(__dirname + '/public'));
+app.post('/signup', upload.none(), (req, res) => {
+  console.log('/signup hit', req.body);
+  let username = req.body.username;
+  let password = req.body.password;
+  if (passwordsAssoc[username] !== undefined) {
+    return res.send('<html><body> Username taken </body></html>');
+  }
+  passwordsAssoc[username] = password;
+  attempts[username] = 0;
+  res.send('<html><body> signup successful </body></html>');
+});
+
+let loginForm = `
+<html><body>
+<div>
+Invalid username or password <a href="/">Go to home</a>
+<form action="/login" method="POST" enctype="multipart/form-data">        
+    <div>Username</div>        
+    <input type="text" name="username"></input>        
+    <div>Password</div>        
+    <input type="text" name="password"></input>        
+    <input type="submit" value="log me in!"></input>        
+</form>  
+</div>
+</body></html>
+`;
+
+app.post('/login', upload.none(), (req, res) => {
+  console.log('/login hit', req.body);
+  let username = req.body.username;
+  let passwordGiven = req.body.password;
+  let expectedPassword = passwordsAssoc[username];
+  attempts[username]++;
+  if (attempts[username] >= 3) {
+    return res.send('<html><body> Account disabled </body></html>');
+  }
+  if (expectedPassword !== passwordGiven) {
+    res.send(loginForm);
+    return;
+  }
+  attempts[username] = 0;
+  res.send('<html><body> login successful </body></html>');
+});
+app.listen(4000, () => {
+  console.log('server started');
+});
