8ec0d79ec717cc4e3f9199ad45b68159071bc54e
Jordan - Instructor <jordan-instructor@Jordans-MacBook-Pro.local>
2019-10-26 12:59:54 -0400

    updated images

--- 2700-dom-reflex-game/public/samurai.js
+++ 2700-dom-reflex-game/public/samurai.js
@@ -10,55 +10,55 @@ const sounds = {
   drum: new Audio('/static/drum.wav'),
 };
 
-document.addEventListener('keydown', (event) => {
-  const key = event.key.toLowerCase();
-  console.log(key);
-  if (won !== '' || (key !== 'p' && key !== 'q')) return;
-
-  if (key === 'p') {
-    won = gameStarted ? 'p' : 'q';
-  }
-  if (key === 'q') {
-    won = gameStarted ? 'q' : 'p';
-  }
-
-  // alternative logic
-  // if (gameStarted) won = key;
-  // else if (key === 'p') won = 'q'
-  // else won = 'p'
-
-  wins[won]++;
-  endRound();
-});
-
 function endRound() {
   stopAudio('drum');
   stopAudio('signal');
   sounds.win.play();
   if (wins.p < ROUNDS && wins.q < ROUNDS) {
     status.textContent = won + ' WON THE ROUND! 🎉';
-    setTimeout(startGame, 1000);
+    setTimeout(startRound, 1000);
   } else {
-    status.innerHTML =
-      won +
-      ' WON THE GAME!  <img src="https://i1.wp.com/azwildlife.org/wp-content/uploads/2018/07/trophy.png?resize=233%2C233&ssl=1" >';
+    status.innerHTML = won + ' WON THE GAME!  <img src="/static/trophy.png" >';
   }
 }
 
-function startGame() {
+function startRound() {
   stopAudio('win');
   sounds.drum.play();
   won = '';
   gameStarted = false;
-  status.innerHTML =
-    '<img src="https://media.giphy.com/media/116seTvbXx07F6/giphy.gif" >';
+  status.innerHTML = '<img src="/static/drumroll.gif" >';
   setTimeout(() => {
     if (won !== '') return;
-    status.innerHTML = '<img src="http://www.orokos.com/odam/kart/go.png" >';
+    status.innerHTML = '<img src="/static/go.png" >';
     stopAudio('drum');
     sounds.signal.play();
     gameStarted = true;
   }, getRandom(500, 1500));
+}
+
+function startGame() {
+  document.addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase();
+    console.log(key);
+    if (won !== '' || (key !== 'p' && key !== 'q')) return;
+
+    if (key === 'p') {
+      won = gameStarted ? 'p' : 'q';
+    }
+    if (key === 'q') {
+      won = gameStarted ? 'q' : 'p';
+    }
+
+    // alternative logic
+    // if (gameStarted) won = key;
+    // else if (key === 'p') won = 'q'
+    // else won = 'p'
+
+    wins[won]++;
+    endRound();
+  });
+  startRound();
 }
 
 // Add a button to trigger the start function otherwise the browser prevents audio from playing.
