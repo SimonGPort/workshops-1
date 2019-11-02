0593a3d90e3b0309e518307798f8c90d0cf0c3ac
Jordan - Instructor <jordan-instructor@jordans-mbp.wireless.concordia.ca>
2019-11-02 13:23:45 -0400

    Add solution for fetch chat

--- 2800-fetch-chat/public/app.js
+++ 2800-fetch-chat/public/app.js
@@ -1,14 +1,107 @@
-let fetchAndUpdate = async () => { 
-    let response = await fetch('/messages') 
-    let responseBody = await response.text() 
-    let parsed = JSON.parse(responseBody) 
-    let msgListUL = document.getElementById('msg-list') 
-    msgListUL.innerHTML = "" 
-    parsed.forEach(elem => { 
-        let li = document.createElement("li") 
-        li.innerText = elem.user + ": " + elem.msg 
-        msgListUL.append(li) 
-    }) 
-} 
-fetchAndUpdate() 
-setInterval(fetchAndUpdate, 500) 
+const loginForm = document.getElementById('login-form');
+const loginUsername = document.getElementById('login-username');
+const loginPassword = document.getElementById('login-password');
+let messageForm;
+let messageInput;
+
+const HTML = `
+<h1 id="topic"></h1>
+  <ul id="msg-list"></ul>
+  <ul id="active-list"></ul>
+  <h3>Update topic</h3>
+  <form action="/set/topic" method="POST" enctype="multipart/form-data">    
+    <input type="text" name="topic"></input>    
+    <input type="submit"></input>    
+  </form>
+  <h3>Send message</h3>
+  <form id="message-form">    
+    <input type="text" id="message-input"></input>    
+    <input type="submit"></input>    
+  </form>
+  <h3>Ignore user</h3>
+  <form action="/set/ignore" method="POST" enctype="multipart/form-data">    
+    <input type="text" name="username"></input>    
+    <input type="submit"></input>    
+  </form>
+  <h3>Update username</h3>
+  <form action="/set/username" method="POST" enctype="multipart/form-data">    
+    <input type="text" name="username"></input>    
+    <input type="submit"></input>    
+  </form>
+  <h3>Update color</h3>
+  <form action="/set/color" method="POST" enctype="multipart/form-data">    
+    <select name="color">
+      <option value="red">Red</option>
+      <option value="pink">Pink</option>
+      <option value="black">Black</option>
+    </select>
+    <input type="submit"></input>    
+  </form>
+`;
+
+const fetchAndUpdate = async () => {
+  const response = await fetch('/messages');
+  const responseBody = await response.text();
+  const parsed = JSON.parse(responseBody);
+  const msgListUL = document.getElementById('msg-list');
+  msgListUL.innerHTML = '';
+  parsed.messages.forEach((message) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<span style="color: ${message.color};">${message.user}</span>: ${message.msg}`;
+    msgListUL.append(li);
+  });
+  const topic = document.getElementById('topic');
+  topic.textContent = parsed.topic;
+  const activeListUL = document.getElementById('active-list');
+  activeListUL.innerHTML = '';
+  //   const activeUsersMap = {};
+  //   parsed.messages
+  //     .filter((message) => Date.now() - message.timestamp <= 5000)
+  //     .forEach((message) => (activeUsersMap[message.user] = true));
+
+  const activeUsers = parsed.messages.reduce((acc, message) => {
+    if (Date.now() - message.timestamp <= 5000) acc[message.user] = true;
+    return acc;
+  }, {});
+  Object.keys(activeUsers).map((username) => {
+    const li = document.createElement('li');
+    li.textContent = username;
+    activeListUL.append(li);
+  });
+};
+
+loginForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData();
+  formData.append('username', loginUsername.value);
+  formData.append('password', loginPassword.value);
+  const res = await fetch('/login', {
+    method: 'POST',
+    body: formData,
+  });
+  const body = await res.text();
+  const parsed = JSON.parse(body);
+  console.log(parsed);
+  if (parsed.success) {
+    document.body.innerHTML = HTML;
+
+    messageForm = document.getElementById('message-form');
+    messageInput = document.getElementById('message-input');
+
+    messageForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append('message', messageInput.value);
+      fetch('/messages', {
+        credentials: 'same-origin',
+        method: 'POST',
+        body: formData,
+      });
+    });
+
+    fetchAndUpdate();
+    setInterval(fetchAndUpdate, 500);
+  } else {
+    alert(parsed.message);
+  }
+});
--- 2800-fetch-chat/server.js
+++ 2800-fetch-chat/server.js
@@ -1,46 +1,129 @@
-let express = require("express") 
-let app = express() 
-let cookieParser = require('cookie-parser') 
-let multer = require("multer") 
-let upload = multer() 
-app.use(cookieParser()) 
-let passwordsAssoc = {} 
-let sessions = {} 
-let messages = [] 
-app.use('/static', express.static(__dirname + '/public')) 
-app.get("/", (req, res) => { 
-  res.sendFile(__dirname + "/public/index.html") 
-}) 
-app.post("/messages", upload.none(), (req, res) => { 
-  console.log('POST messages body', req.body) 
-  let newMessage = { 
-    user: sessions[req.cookies["sid"]], 
-    msg: req.body.message 
-  } 
-  messages.push(newMessage) 
-  res.sendFile(__dirname + '/public/chat.html') 
-}) 
-app.get("/messages", (req, res) => { 
-  console.log('Sending back the messages') 
-  res.send(JSON.stringify(messages)) 
-}) 
-app.post("/signup", upload.none(), (req, res) => { 
-  let username = req.body.username 
-  let password = req.body.password 
-  passwordsAssoc[username] = password 
-  res.send("<html><body> signup successful </body></html>") 
-}) 
-app.post("/login", upload.none(), (req, res) => { 
-  let username = req.body.username 
-  let passwordGiven = req.body.password 
-  let expectedPassword = passwordsAssoc[username] 
-  if (expectedPassword !== passwordGiven) { 
-    res.send("<html><body> invalid username or password </body></html>") 
-    return 
-  } 
-  let sid = Math.floor(Math.random() * 10000000) 
-  sessions[sid] = username 
-  res.cookie('sid', sid) 
-  res.sendFile(__dirname + '/public/chat.html') 
-}) 
-app.listen(4000) 
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const upload = multer();
+app.use(cookieParser());
+const passwordsAssoc = {};
+const sessions = {};
+const messages = [];
+const colors = {};
+const ignored = {};
+// const timestamps = {};
+let topic = '';
+
+// const users = {};
+// { bob: { username: 'bob', color: 'red', ignored: []}}
+
+app.use('/static', express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+app.post('/messages', upload.none(), (req, res) => {
+  const username = sessions[req.cookies.sid];
+
+  // const user = sessions[req.cookies.sid];
+  // { username: 'bob', color: 'red' }
+
+  console.log('POST messages body', req.body);
+  const newMessage = {
+    user: username,
+    msg: req.body.message,
+    color: colors[username],
+    timestamp: Date.now(),
+  };
+
+  // const newMessage = {
+  //   user: user,
+  //   msg: req.body.message,
+  // };
+
+  messages.push(newMessage);
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.get('/messages', (req, res) => {
+  const sessionId = req.cookies.sid;
+  const username = sessions[sessionId];
+  console.log('Sending back the messages');
+  const filteredMessages = messages.filter(
+    (message) => !ignored[username].includes(message.user)
+  );
+  res.send(JSON.stringify({ messages: filteredMessages, topic }));
+});
+
+app.post('/set/username', upload.none(), (req, res) => {
+  const sessionId = req.cookies.sid;
+  const currentUsername = sessions[sessionId];
+  const newUsername = req.body.username;
+  if (passwordsAssoc[newUsername] !== undefined) {
+    return res.send('<html><body> Username taken </body></html>');
+  }
+  passwordsAssoc[newUsername] = passwordsAssoc[currentUsername];
+  delete passwordsAssoc[currentUsername];
+  sessions[sessionId] = newUsername;
+  messages.forEach((message) => {
+    if (message.user === currentUsername) message.user = newUsername;
+  });
+  ignored[newUsername] = ignored[currentUsername];
+  delete ignored[currentUsername];
+  colors[newUsername] = colors[currentUsername];
+  delete colors[currentUsername];
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.post('/set/color', upload.none(), (req, res) => {
+  const sessionId = req.cookies.sid;
+  const username = sessions[sessionId];
+  // const user = sessions[req.cookies.sid];
+  const color = req.body.color;
+  colors[username] = color;
+  // user.color = color;
+  messages.forEach((message) => {
+    if (message.user === username) message.color = color;
+  });
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.post('/set/ignore', upload.none(), (req, res) => {
+  const sessionId = req.cookies.sid;
+  const username = sessions[sessionId];
+  const annoyingUser = req.body.username;
+  ignored[username].push(annoyingUser);
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.post('/set/topic', upload.none(), (req, res) => {
+  topic = req.body.topic;
+  res.sendFile(__dirname + '/public/chat.html');
+});
+
+app.post('/signup', upload.none(), (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  if (passwordsAssoc[username] !== undefined) {
+    return res.send('<html><body> Username taken </body></html>');
+  }
+  passwordsAssoc[username] = password;
+  colors[username] = 'black';
+  ignored[username] = [];
+  res.send('<html><body> signup successful </body></html>');
+});
+app.post('/login', upload.none(), (req, res) => {
+  let username = req.body.username;
+  let passwordGiven = req.body.password;
+  let expectedPassword = passwordsAssoc[username];
+  if (expectedPassword !== passwordGiven) {
+    return res.send(
+      JSON.stringify({
+        success: false,
+        message: 'Invalid username or password',
+      })
+    );
+  }
+  let sid = Math.floor(Math.random() * 10000000);
+  sessions[sid] = username;
+  res.cookie('sid', sid);
+  res.send(JSON.stringify({ success: true }));
+});
+app.listen(4000);
