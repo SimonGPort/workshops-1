bfb1affadfeeb3d22fdd369aa82eb592a754960d
Jordan - Instructor <jordan-instructor@Jordans-MacBook-Pro.local>
2019-12-03 19:23:32 -0500

    Add solution react router intro

--- 3400-react-router-intro/src/App.jsx
+++ 3400-react-router-intro/src/App.jsx
@@ -1,33 +1,81 @@
-import React, { Component } from 'react' 
-import { BrowserRouter, Route, Link } from 'react-router-dom' 
-let renderRoot = () => { 
-    return (<div> 
-        Welcome to this page. Click here to view all the items: 
-        <Link to="/allItems">All items</Link> 
-    </div>) 
-} 
-let renderAllItems = () => { 
-    return (<div> 
-        <div><Link to="/item/abc">A hat</Link></div> 
-        <div><Link to="/item/def">A boat</Link></div> 
-    </div>) 
-} 
-let renderItem = routerData => { 
-    if (routerData.match.params.itemId === "abc") { 
-        return (<div>A very nice hat. Only 4 remaining <img src="/hat.jpg" /></div>) 
-    } 
-    if (routerData.match.params.itemId === "def") { 
-        return (<div>A boat. Only 4 remaining <img src="/boat.jpg" /></div>) 
-    } 
-    return (<div> Unknown item </div>) 
-} 
-class App extends Component { 
-    render() { 
-        return (<BrowserRouter><div> 
-            <Route exact={true} path='/' render={renderRoot} /> 
-            <Route exact={true} path='/allItems' render={renderAllItems} /> 
-            <Route exact={true} path='/item/:itemId' render={renderItem} /> 
-        </div></BrowserRouter>) 
-    } 
-} 
-export default App 
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import { items, sellers } from "./data.js";
+
+let renderRoot = () => {
+  return (
+    <div>
+      Welcome to this page. Click here to view all the items:
+      <Link to="/allItems">All items</Link>
+    </div>
+  );
+};
+let renderAllItems = () => {
+  return (
+    <div>
+      {items.map(item => (
+        <div>
+          <div className="item-card">
+            <div className="link-wrapper">
+              <Link to={`/item/${item.id}`}>{item.description}</Link>
+              <Link to={`/seller/${item.sellerId}`}>Go to seller</Link>
+            </div>
+            <img src={item.img} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+let renderItem = routerData => {
+  const item = items.find(item => item.id === routerData.match.params.itemId);
+  if (item === undefined) return <div>Item not found</div>;
+  return (
+    <div className="item-wrapper">
+      {item.description}. Only {item.inventory} remaining
+      <div>
+        <img src={item.img} />
+      </div>
+      <h3>Details</h3>
+      <ul>
+        <li>Price: {item.price}</li>
+        <li>Color: {item.color}</li>
+      </ul>
+    </div>
+  );
+};
+let renderSeller = routerData => {
+  const sellerId = routerData.match.params.sellerId;
+  const seller = sellers.find(seller => seller.id === sellerId);
+  if (seller === undefined) return <div>Seller not found</div>;
+  const sellerItems = items.filter(item => item.sellerId === seller.id);
+  return (
+    <div>
+      <h3>Name: {seller.name}</h3>
+      <p>{seller.description}</p>
+      <h3>Items for sale:</h3>
+      <ul>
+        {sellerItems.map(item => (
+          <li>
+            <Link to={`/item/${item.id}`}>{item.description}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path="/" render={renderRoot} />
+          <Route exact={true} path="/allItems" render={renderAllItems} />
+          <Route exact={true} path="/item/:itemId" render={renderItem} />
+          <Route exact={true} path="/seller/:sellerId" render={renderSeller} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+export default App;
---
+++ 3400-react-router-intro/src/data.js
@@ -0,0 +1,44 @@
+export const items = [
+  {
+    id: "abc",
+    description: "A very nice hat",
+    inventory: 4,
+    img: "/hat.jpg",
+    price: "49$",
+    color: "White",
+    sellerId: "1"
+  },
+  {
+    id: "def",
+    description: "A boat",
+    inventory: 4,
+    img: "/boat.jpg",
+    price: "100000$",
+    color: "White",
+    sellerId: "1"
+  },
+  {
+    id: "lawn",
+    description: "A great lawnmower",
+    inventory: 999,
+    img: "/lawnmower.jpg",
+    price: "999$",
+    color: "Absolutely red",
+    sellerId: "2"
+  },
+  {
+    id: "snow",
+    description: "Snow shoes",
+    inventory: 100,
+    img: "/snowshoes.jpg",
+    price: "79$",
+    color: "Absolutely red",
+    sellerId: "3"
+  }
+];
+
+export const sellers = [
+  { id: "1", name: "Bob", description: "I sell many things" },
+  { id: "2", name: "Max", description: "Everything premium" },
+  { id: "3", name: "Sue", description: "Great deals" }
+];
