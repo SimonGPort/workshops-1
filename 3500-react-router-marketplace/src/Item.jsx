b35f623d8b0d9146ee619f003b06349264b7747d
Jordan - Instructor <jordan-instructor@Jordans-MacBook-Pro.local>
2019-12-10 20:00:29 -0500

    Add solution for react router marketplace

--- 3500-react-router-marketplace/src/Item.jsx
+++ 3500-react-router-marketplace/src/Item.jsx
@@ -1,16 +1,23 @@
-import React, { Component } from 'react'; 
-import { Link } from 'react-router-dom' 
-import './App.css'; 
-class Item extends Component {  
-  render() { 
-    return (<div className="card center "> 
-      <img height="100px" src={this.props.imageLocation} /> 
-      <div> 
-        <div>{this.props.description}</div> 
-        <div>{this.props.cost}</div> 
-        <Link to={"/seller/" + this.props.sellerId}> Link to seller </Link> 
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Item extends Component {
+  render() {
+    const { image, price, description, sellerId, id } = this.props.item;
+    return (
+      <div className="card center">
+        <img height="100px" src={image} />
+        <div>
+          <div>{description}</div>
+          <div>{price}</div>
+          <div>
+            <Link to={"/seller/" + sellerId}> Link to seller </Link>
+          </div>
+          <div>
+            <Link to={"/item/" + id}> Link to item </Link>
+          </div>
+        </div>
       </div>
-    </div>) 
-  } 
-} 
-export default Item; 
+    );
+  }
+}
+export default Item;
